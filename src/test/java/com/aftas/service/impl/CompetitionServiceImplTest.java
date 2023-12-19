66b7b934b129c1e254aa09ef2db039df5a55f721
MohamedJamh <jam7.mohamed@gmail.com>
2023-12-19 13:56:50 +0100

    main : add - working on tests , competition creation , assert error throw on invalid date (less than two days from now) , invalid time (star time after end time) , assert pass on vali

--- src/test/java/com/aftas/service/impl/CompetitionServiceImplTest.java
+++ src/test/java/com/aftas/service/impl/CompetitionServiceImplTest.java
@@ -14,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.doNothing;
@@ -42,11 +43,10 @@ class CompetitionServiceImplTest {
                 .numberOfParticipants(0)
                 .build();
     }
-    @DisplayName("create competition in invalid date should throw error")
+    @DisplayName("create competition in occupied date should throw error")
     @Test
     void createCompetition_saveCompetitionInvalidDate_throwsError() {
         Competition invalidDateTimeCompetition = Competition.builder()
-                .code("ACD-1-1-21")
                 .date(competition.getDate()) // same date as competition returned by repository mock
                 .build();
         when(competitionRepository.getCompetitionByDate(invalidDateTimeCompetition.getDate())).thenReturn(java.util.Optional.of(competition));
@@ -54,4 +54,47 @@ class CompetitionServiceImplTest {
         ValidationException exception = assertThrows(ValidationException.class, () -> competitionService.createCompetition(invalidDateTimeCompetition));
         assertEquals("There is already a competition on this day", exception.getErrorMessage().getMessage());
     }
+
+    @DisplayName("create competition in invalid start / end time should throw error")
+    @Test
+    void createCompetition_saveCompetitionInvalidTime_throwsError() {
+        Competition invalidTimeCompetition = Competition.builder()
+                .date(LocalDate.now().plusDays(3))
+                .startTime(LocalTime.now().plusHours(1)) // start time after end time
+                .endTime(LocalTime.now())
+                .build();
+        when(competitionRepository.getCompetitionByDate(invalidTimeCompetition.getDate())).thenReturn(Optional.empty());
+        ValidationException exception = assertThrows(ValidationException.class, () -> competitionService.createCompetition(invalidTimeCompetition));
+        assertEquals("Start time cannot be equal or after end time", exception.getErrorMessage().getMessage());
+    }
+
+    @DisplayName("create competition in invalid date (less than 2 days from now) should throw error")
+    @Test
+    void createCompetition_saveCompetitionInvalidDate_throwsError2() {
+        Competition invalidDateCompetition = Competition.builder()
+                .date(LocalDate.now().plusDays(1)) // date less than 2 days from now
+                .startTime(LocalTime.now())
+                .endTime(LocalTime.now().plusHours(2))
+                .build();
+        when(competitionRepository.getCompetitionByDate(invalidDateCompetition.getDate())).thenReturn(Optional.empty());
+        ValidationException exception = assertThrows(ValidationException.class, () -> competitionService.createCompetition(invalidDateCompetition));
+        assertEquals("Competition date cannot be less than 2 days from now", exception.getErrorMessage().getMessage());
+    }
+
+    @DisplayName("create competition should pass with no thrown error")
+    @Test
+    void createCompetition_saveCompetitionValid_shouldPass() throws ValidationException {
+        Competition validCompetition = Competition.builder()
+                .location("somewhere")
+                .numberOfParticipants(0)
+                .date(LocalDate.now().plusDays(3))
+                .startTime(LocalTime.now())
+                .endTime(LocalTime.now().plusHours(2))
+                .build();
+        when(competitionRepository.getCompetitionByDate(validCompetition.getDate())).thenReturn(Optional.empty());
+        when(competitionRepository.save(validCompetition)).thenReturn(validCompetition);
+        Competition savedCompetition = competitionService.createCompetition(validCompetition);
+        assertEquals(validCompetition, savedCompetition);
+    }
+
 }
