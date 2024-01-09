ba499ee900a0ca9134aaf8b228a29998d2d6ea19
MohamedJamh <jam7.mohamed@gmail.com>
2024-01-09 14:33:54 +0100

    main : add - add ability to enable and disable seeders on app started based on application.yml

--- src/main/java/com/aftas/seeder/AppSeeder.java
+++ src/main/java/com/aftas/seeder/AppSeeder.java
@@ -3,18 +3,22 @@ package com.aftas.seeder;
 import com.aftas.seeder.dbSeeders.FishSeeder;
 import com.aftas.seeder.dbSeeders.LevelSeeder;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppSeeder {
     private final FishSeeder fishSeeder;
     private final LevelSeeder levelSeeder;
+    @Value("${seeders.enabled}")
+    private Boolean seed;
     public AppSeeder(FishSeeder fishSeeder, LevelSeeder levelSeeder) {
         this.fishSeeder = fishSeeder;
         this.levelSeeder = levelSeeder;
     }
     @PostConstruct
     public void init() {
+        if(Boolean.FALSE.equals(seed)) return;
         levelSeeder.seed();
         fishSeeder.seed();
     }
